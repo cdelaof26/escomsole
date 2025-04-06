472990941e29275bddc6adc93eb7e85a597b85fc
cdelaof26 <cdelaof1800@alumno.ipn.mx>
2025-04-05 19:55:00 -0600

    Function to calculate first set

--- interpreter/SyntacticGrammar.java
+++ interpreter/SyntacticGrammar.java
@@ -1,5 +1,6 @@
 package interpreter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import modeling.NonTerminal;
 import modeling.TokenType;
@@ -9,9 +10,44 @@ import modeling.TokenType;
  * @author cristopher
  */
 public class SyntacticGrammar {
-    public static HashMap<NonTerminal, Object[][]> rules;
+    public static final HashMap<NonTerminal, Object[][]> rules = new HashMap<>();
     
     static {
+        
+        /**
+         * Testing grammar 1
+         * E -> TE'
+         * E' -> +TE'|eps
+         * T -> FT'
+         * T' -> *FT'|eps
+         * F -> (E)|id
+         * 
+         * NonTerminal: E, T, E_P, F, T_P,
+         *
+        rules.put(NonTerminal.E, new Object[][]{{NonTerminal.T, NonTerminal.E_P}});
+        rules.put(NonTerminal.E_P, new Object[][]{{TokenType.ESC_PLUS, NonTerminal.T, NonTerminal.E_P}, null});
+        rules.put(NonTerminal.T, new Object[][]{{NonTerminal.F, NonTerminal.T_P}});
+        rules.put(NonTerminal.T_P, new Object[][]{{TokenType.ESC_STAR, NonTerminal.F, NonTerminal.T_P}, null});
+        rules.put(NonTerminal.F, new Object[][]{{TokenType.ESC_LEFT_PAREN, NonTerminal.E, TokenType.ESC_RIGHT_PAREN}, {TokenType.ESC_IDENTIFIER}});
+        // */
+        
+        /**
+         * Testing grammar 2
+         * A -> Aa|BCD
+         * B -> b|eps
+         * C -> c|eps
+         * D -> d|Ce
+         * 
+         * NonTerminal: A, B, C, D,
+         * TokenType: a, b, c, d, e,
+         *
+        rules.put(NonTerminal.A, new Object[][]{{NonTerminal.A, TokenType.a}, {NonTerminal.B, NonTerminal.C, NonTerminal.D}});
+        rules.put(NonTerminal.B, new Object[][]{{TokenType.b}, null});
+        rules.put(NonTerminal.C, new Object[][]{{TokenType.c}, null});
+        rules.put(NonTerminal.D, new Object[][]{{TokenType.d}, {NonTerminal.C, TokenType.e}});
+        // */
+        
+        
         rules.put(NonTerminal.PROGRAM, new Object[][]{{NonTerminal.DECLARATION}});
         
         
@@ -21,7 +57,7 @@ public class SyntacticGrammar {
             {NonTerminal.FUN_DECL, NonTerminal.DECLARATION},
             {NonTerminal.VAR_DECL, NonTerminal.DECLARATION},
             {NonTerminal.STATEMENT, NonTerminal.DECLARATION},
-            {}
+            null
         });
         
         rules.put(NonTerminal.FUN_DECL, new Object[][]{
@@ -32,7 +68,7 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.VAR_INIT, new Object[][]{
             {TokenType.ESC_EQUAL, NonTerminal.EXPRESSION}, 
-            {}
+            null
         });
         
         
@@ -69,7 +105,7 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.FOR_STMT_INC, new Object[][] {
             {NonTerminal.EXPRESSION},
-            {}
+            null
         });
         
         rules.put(NonTerminal.IF_STMT, new Object[][] {
@@ -78,7 +114,7 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.ELSE_STATEMENT, new Object[][] {
             {TokenType.ESC_ELSE, NonTerminal.STATEMENT}, 
-            {}
+            null
         });
         
         rules.put(NonTerminal.PRINT_STMT, new Object[][] {{NonTerminal.EXPRESSION, TokenType.ESC_SEMICOLON}});
@@ -87,7 +123,7 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.RETURN_EXP_OPC, new Object[][] {
             {NonTerminal.EXPRESSION}, 
-            {}
+            null
         });
         
         rules.put(NonTerminal.WHILE_STMT, new Object[][] {
@@ -105,21 +141,21 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.ASSIGNMENT_OPC, new Object[][] {
             {TokenType.ESC_EQUAL, NonTerminal.EXPRESSION},
-            {}
+            null
         });
         
         rules.put(NonTerminal.LOGIC_OR, new Object[][] {{NonTerminal.LOGIC_AND, NonTerminal.LOGIC_OR_P}});
         
         rules.put(NonTerminal.LOGIC_OR_P, new Object[][] {
             {TokenType.ESC_OR, NonTerminal.LOGIC_OR},
-            {}
+            null
         });
         
         rules.put(NonTerminal.LOGIC_AND, new Object[][] {{NonTerminal.EQUALITY, NonTerminal.LOGIC_AND_P}});
         
         rules.put(NonTerminal.LOGIC_AND_P, new Object[][] {
             {TokenType.ESC_AND, NonTerminal.LOGIC_AND},
-            {}
+            null
         });
         
         rules.put(NonTerminal.EQUALITY, new Object[][] {{NonTerminal.COMPARISON, NonTerminal.EQUALITY_P}});
@@ -127,7 +163,7 @@ public class SyntacticGrammar {
         rules.put(NonTerminal.EQUALITY_P, new Object[][] {
             {TokenType.ESC_NOT_EQUAL, NonTerminal.EQUALITY},
             {TokenType.ESC_EQUAL_EQUAL, NonTerminal.EQUALITY},
-            {}
+            null
         });
         
         rules.put(NonTerminal.COMPARISON, new Object[][] {{NonTerminal.TERM, NonTerminal.COMPARISON_P}});
@@ -137,7 +173,7 @@ public class SyntacticGrammar {
             {TokenType.ESC_GREATER_EQUAL, NonTerminal.COMPARISON},
             {TokenType.ESC_LESS, NonTerminal.COMPARISON},
             {TokenType.ESC_LESS_EQUAL, NonTerminal.COMPARISON},
-            {}
+            null
         });
         
         rules.put(NonTerminal.TERM, new Object[][] {{NonTerminal.FACTOR, NonTerminal.TERM_P}});
@@ -145,7 +181,7 @@ public class SyntacticGrammar {
         rules.put(NonTerminal.TERM_P, new Object[][] {
             {TokenType.ESC_MINUS, NonTerminal.TERM},
             {TokenType.ESC_PLUS, NonTerminal.TERM},
-            {}
+            null
         });
         
         rules.put(NonTerminal.FACTOR, new Object[][] {
@@ -155,7 +191,7 @@ public class SyntacticGrammar {
         rules.put(NonTerminal.FACTOR_P, new Object[][] {
             {TokenType.ESC_SLASH, NonTerminal.FACTOR},
             {TokenType.ESC_STAR, NonTerminal.FACTOR},
-            {}
+            null
         });
         
         rules.put(NonTerminal.UNARY, new Object[][] {
@@ -168,7 +204,7 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.CALL_P, new Object[][] {
             {TokenType.ESC_LEFT_PAREN, NonTerminal.ARGUMENTS, TokenType.ESC_RIGHT_PAREN},
-            {}
+            null
         });
         
         rules.put(NonTerminal.PRIMARY, new Object[][] {
@@ -186,22 +222,77 @@ public class SyntacticGrammar {
         
         rules.put(NonTerminal.PARAMETERS, new Object[][] {
             {TokenType.ESC_IDENTIFIER, NonTerminal.PARAMETERS_P},
-            {}
+            null
         });
         
         rules.put(NonTerminal.PARAMETERS_P, new Object[][] {
             {TokenType.ESC_COMMA, TokenType.ESC_IDENTIFIER, NonTerminal.PARAMETERS_P},
-            {}
+            null
         });
         
         rules.put(NonTerminal.ARGUMENTS, new Object[][] {
             {NonTerminal.EXPRESSION, NonTerminal.ARGUMENTS_P},
-            {}
+            null
         });
         
         rules.put(NonTerminal.ARGUMENTS_P, new Object[][] {
             {TokenType.ESC_COMMA, NonTerminal.EXPRESSION, NonTerminal.ARGUMENTS_P},
-            {}
+            null
         });
+        
+        // */
+    }
+
+    // Pseudo code extracted from
+    // https://campusvirtual.ull.es/ocw/pluginfile.php/3850/mod_resource/content/0/perlexamples/node194.html
+    // 2012-05-22
+    //
+    public static ArrayList<Object> first(Object alpha, Object prev) {
+//        System.out.println("alpha = " + alpha);
+//        System.out.println("prev = " + prev);
+
+        ArrayList<Object> set = new ArrayList<>();
+        
+        // This prevents stack overflow
+        if (alpha.equals(prev))
+            return set;
+        
+        if (alpha instanceof TokenType) {
+            set.add(alpha);
+        } else {
+            Object [][] productions = rules.get((NonTerminal) alpha);
+            for (Object [] p : productions) {
+                if (p == null) {
+                    if (!set.contains(p))
+                        set.add(p);
+                    
+                    continue;
+                }
+                
+                boolean containsEpsilon = false;
+                for (Object e : p) {
+                    for (Object o : first(e, alpha)) {
+                        if (o == null) {
+                            containsEpsilon = true;
+                            continue;
+                        }
+                        
+                        if (!set.contains(o))
+                            set.add(o);
+                    }
+                    
+                    if (!containsEpsilon)
+                        break;
+                }
+                
+//                if (!containsEpsilon)
+//                    break;
+            }
+            
+            if (!set.contains(null) && productions[productions.length - 1] == null)
+                set.add(null);
+        }
+        
+        return set;
     }
 }
